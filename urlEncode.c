bc17062f8de7ad5dbc5f379db6e09e814a4678b8
hej123456 <liljeberg.anton@gmail.com>
2014-04-23 16:01:16 +0200

    urlEncode

---
+++ urlEncode.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "urlEncode.h"
+
+/* Example
+int main()
+{
+	unsigned char str[] = "http://hej hej";
+	char encodedStr[sizeof(str) * 3];
+
+	urlEncode(str, encodedStr);
+
+	printf("%s", encodedStr);
+	return 0;
+}*/
+
+
+void urlEncode(unsigned char *hash, char *newStr)
+{
+    int i;
+    char tbl[256];
+    for(i=0; i<256; i++)
+        tbl[i] = isalnum(i)||i == '~'||i == '-'||i == '.'||i == '_' ? i : 0;
+
+    char *ptbl = tbl;
+
+	for (; *hash; hash++) {
+		ptbl[*hash] ? sprintf(newStr, "%c", ptbl[*hash]) : sprintf(newStr, "%%%02X", *hash);
+		while (*++newStr);
+	}
+}
+
---
+++ urlEncode.h
@@ -0,0 +1,9 @@
+#ifndef _regexing_h
+#define _regexing_h
+
+#include <stdio.h>
+#include <ctype.h>
+
+void urlEncode(unsigned char *hash, char *newStr);
+
+#endif
