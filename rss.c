46d051ef9683ddd65ca08ad938f30cbfa4142db2
hej123456 <liljeberg.anton@gmail.com>
2014-04-30 12:28:52 +0200

    rss t2

--- rss.c
+++ rss.c
@@ -18,11 +18,11 @@ int getFeed(char *destAddr, char *dir){
 	struct hostent *server;
 	struct sockaddr_in serverAddr;
 	int s, slen=sizeof(server);
-	char request[];
+	char request[] "GET /feeds/27.rss HTTP/1.1\r\nHost: showrss.info\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n";
 	char buffer[100];
 
-	request = malloc(sizeof(dir)+sizeof(destAddr)+16*sizeof(char));
-	sprinf(request, "GET%cHTTP/1.1Host:%s", dir, destAddr);
+	//request = malloc(sizeof(dir)+sizeof(destAddr)+16*sizeof(char));
+	//sprintf(request, "GET%cHTTP/1.1Host:%c", dir, destAddr);
 
 	if((s=socket(AF_INET, SOCK_STREAM, 0))==-1) return 0;
 
@@ -30,12 +30,13 @@ int getFeed(char *destAddr, char *dir){
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(80);
 
-	if(connect(s, &serverAddr, sizeof(serverAddr))<0) return 0;
+	if(connect(s, (struct serveraddr*) &serverAddr, sizeof(serverAddr))<0) return 0;
 	if(send(s, request, sizeof(request), 0)<0) return 0;
 
 	while(recv(s, buffer, 100, 0)){
 		puts(buffer);
 	}
+	puts(buffer);
 	close(s);
 	return 1;
 }
