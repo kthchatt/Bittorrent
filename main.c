161770cd37c44622d35dbfbd4338f1253d2bae1c
gustafni <gustafni@kth.se>
2014-05-07 16:01:02 +0200

    lots of  new stuff

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 //gcc main.c tracker.c announce.c bencodning.c scrape.c swarm.c peerwire.c urlparse.c -o main.out -pthread -lssl -lcrypto
-
+#define MAX_TORRENTS 100
 #include "tracker.h"
 #include "bencodning.h"
 
+torrent_info *torrents[MAX_TORRENTS];
+
 int main (int argc, char *argv[]){
 	int i;
-	torrent_info data;
+	torrents[0] = malloc(sizeof(torrent_info));
 
 	char *trackers[MAX_TRACKERS];
 	for (i = 0; i < MAX_TRACKERS; i++){
-		trackers[i] = data._announce_list[i];
+		trackers[i] = torrents[0]->_announce_list[i];
 	}
 
 	fprintf(stderr, "This is main\n");
-	decode_bencode(argv[1], &data);
+	decode_bencode(argv[1], torrents[0]);
 	fprintf(stderr, "Tracker no:0 = %s\n", trackers[0]);
-	//track(data._info_hash,trackers);
-	track("00000000000000000000",trackers);
+	track(torrents[0]->_info_hash,trackers);
+	//track("00000000000000000000",trackers);
 	fprintf(stderr, "Tracker is running \n");
 	sleep(30);
 	//untrack("00000000000000000000");
