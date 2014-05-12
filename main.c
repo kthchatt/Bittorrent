387dc396d55f634d18a84b4a60f53ba49ee9f2e2
gustafni <gustafni@kth.se>
2014-05-12 14:39:54 +0200

    changes is writepiece, see TODO

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-//gcc main.c tracker.c announce.c bencodning.c scrape.c swarm.c peerwire.c urlparse.c createfile.c -o main.out -pthread -lssl -lcrypto
+//gcc main.c tracker.c announce.c bencodning.c scrape.c swarm.c peerwire.c urlparse.c createfile.c writepiece.c -o main.out -pthread -lssl -lcrypto
 
 #define MAX_TORRENTS 100
 #include "tracker.h"
 #include "bencodning.h"
 #include "torrent.h"
 #include "createfile.h"
+#include "writepiece.h"
 
 
 torrent_info *torrents[MAX_TORRENTS];
@@ -25,6 +26,11 @@ int main (int argc, char *argv[]){
 	fprintf(stderr, "Tracker no:0 = %s\n", trackers[0]);
 
 	create_file(torrents[0]);
+
+	void *ptr = malloc(torrents[0]->_piece_length);
+	memset(ptr, 'A', torrents[0]->_piece_length);
+	write_piece(torrents[0], ptr);
+
 	/*
 	track(torrents[0]->_info_hash,trackers);
 	//track("00000000000000000000",trackers);
--- writepiece.c
+++ writepiece.c
@@ -1,5 +1,7 @@
 /*
 */
+#include "writepiece.h"
+#include "bencodning.h"
 
 int write_piece(torrent_info *torrent, void *piece){
 	int i, number_of_pieces, start, stop, bytes_to_load = 0, start_from_file, back_from_EOF = 0;
@@ -9,18 +11,19 @@ int write_piece(torrent_info *torrent, void *piece){
 	SHA1(piece, torrent->_piece_length, hash);
 	number_of_pieces = (int) torrent->_hash_length /20;
 	for (i = 0; i < number_of_pieces; i++){
-		if (strncpy(hash, torrent->_pieces[i]) == 0){
+		if (strncpy(hash, torrent->_pieces[i], 20) == 0){
 			found_piece = i;
 			break;
 		}
 	}
 	if (found_piece == -1){
 		fprintf(stderr, "The hash is not found\n");
-		return 1;
+		//return 1;
 	}
+	found_piece= 0;
 
 	start = torrent->_piece_length * found_piece;
-	sopt = start + torrent->_piece_length;
+	stop = start + torrent->_piece_length;
 	i = 0;
 	if (start != 0){
 		while (i < torrent->_number_of_files){
@@ -38,29 +41,40 @@ int write_piece(torrent_info *torrent, void *piece){
 	} else {
 		//TODO, Add code to handle if it is fist pirece. 
 	}
+	fprintf(stderr, "Will need to open :%d files\n", i);
 	first_file_to_open = i;
 
 	//Find last_file_to_open. 
-	FILE *fp
+	FILE *fp;
+
+	//TODO, fopen cant handle spaces. 
 	fp = fopen(torrent->_file_path[first_file_to_open], "w+");
+	if (fp == NULL){
+		fprintf(stderr, "Error opening file\n");
+		return 1;
+	}
+	fprintf(stderr, "File %s is open\n", torrent->_file_path[first_file_to_open]);
 	fseek(fp, (-back_from_EOF), SEEK_END);
 	fprintf(stderr, "current possition in file :%li\n", ftell(fp));
 	while(!feof(fp)){
-		fprintf(stderr, "%c", piece);
+		fwrite(piece, 1, 1, fp);
+		//fprintf(stderr, "%c", (char) *piece);
 		(char *) piece++;
 		bytes_to_write--;
 	}
 	first_file_to_open++;
 	last_file_to_open = first_file_to_open;
-	while(bytes_to_write <= 0){
-		
+	while(bytes_to_write > 0){
+		fp = fopen(torrent->_file_path[first_file_to_open], "w+");
+		while(!feof(fp) && bytes_to_write != 0){
+			fwrite(piece, 1, 1, fp);
+			//fprintf(stderr, "%c", (char) *piece);
+			(char *) piece++;
+			bytes_to_write--;
+		}
+		first_file_to_open++;
 	}
 
-	/*while (total_to_load < piece_length && fileindex < torrent->_number_of_files){
-    		total_to_load = total_to_load + torrent->_file_length[fileindex];
-    		nom_of_files_to_load++;
-    		fileindex++;
-    }*/
-
 	free (piece);
+	return 0;
 }
