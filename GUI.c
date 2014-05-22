10042182101ff1d72a1f1bf677435a461548f810
chilimannen <chilimannen93@yahoo.com>
2014-05-22 10:04:34 +0200

    rss.

--- GUI.c
+++ GUI.c
@@ -9,6 +9,7 @@
 #include "bencodning.h"
 #include "protocol_meta.h"
 #include "urlparse.h"
+#include "rss2.h"
 
 /*
 	include swarm & netstat, the fileman should be included too. ~RD
@@ -71,6 +72,8 @@
  	STATE_CREATING,
  };
 
+ //todo: if torrentlist is CREATING and tinfo.created == true, set as downloading and track.
+
 enum {
 	COL_ID = 0,
 	COL_NAME = 1,
@@ -106,7 +109,7 @@ static char* COLUMN_NAME[] = {"#", "Name", "Size", "Done", "Status", "Download",
 //dynamic array of torrentlist.  ~RD
 torrentlist_t* torrentlist; 
 int torrentlist_count;
-pthread_t update_thread, motd_thread;
+pthread_t update_thread, motd_thread, rss_thread;
 
 //global required, multiple pointers to retain references. ~RD
 GtkWidget *tv_inactive, *tv_downloading, *tv_completed, *tv_seeding;
@@ -114,6 +117,8 @@ GtkWidget *tlb_inactive, *tlb_downloading, *tlb_completed, *tlb_seeding;
 GtkListStore *md_inactive, *md_downloading, *md_completed, *md_seeding;
 GtkWidget *lb_netstat, *lb_motd;
 GtkWidget *notebook;
+rss_t rssfeed;
+
 
 //calculate the filesize from bencodning.c .. ~RD
 void torrent_size(torrent_info* tinfo, char* filesize)
@@ -333,7 +338,30 @@ void* motd_timer_thread(void* arg)
 
 	MOTD_fetch(response);
 	sleep(MOTD_TIMEOUT + 1);			//wait timeout.
-	gtk_label_set_text((GtkLabel*) lb_motd, response);		//todo: fix the design, updating GtkLabel isn't pretty.
+	gtk_label_set_text((GtkLabel*) lb_motd, response);	
+	return NULL;
+}
+
+//a timer for updating the MOTD after the retrieval timeout has passed. ~RD
+void* rss_timer_thread(void* arg)
+{
+	//global variables allows the changing of source. ~RD
+char* host = "showrss.info";
+char* uri = "/feeds/27.rss";
+
+	rssfeed.host = malloc(MAX_URL_LEN);
+	rssfeed.uri = malloc(MAX_URL_LEN);
+	strcpy(rssfeed.host, "showrss.info");
+	strcpy(rssfeed.uri, "/feeds/27.rss");
+
+	rss_fetch(&rssfeed);
+	sleep(RSS_TIMEOUT + 1);			//wait timeout.
+	
+	//read rss feed here.
+	int i;
+	for (i = 0; i < rssfeed.item_count; i++)
+		printf("\nTitle: %s, Description: %s, Link: %s", rssfeed.item[i].title, rssfeed.item[i].description, rssfeed.item[i].link);
+
 	return NULL;
 }
 
@@ -669,7 +697,7 @@ void add_torrent(){
 }
 
 void MOTD(GtkWidget **label, GtkWidget **table) {
-	gtk_label_set_width_chars(GTK_LABEL(*label), 100);  //???
+	gtk_label_set_width_chars(GTK_LABEL( (GtkLabel*) *label), 100);  //???
 	*label = gtk_label_new("Loading MOTD ..."); // Label content
   	gtk_misc_set_alignment(GTK_MISC(*label), 0, 1); // Sets alignment of label
   	gtk_table_attach_defaults(GTK_TABLE(*table), *label, 0, 4, 10, 11); 
@@ -837,6 +865,9 @@ int main (int argc, char *argv[])
 
 	if (!(pthread_create(&motd_thread, NULL, motd_timer_thread, NULL)))
 			printf("\nWaiting for MOTD in Thread.");
+
+	if (!(pthread_create(&rss_thread, NULL, rss_timer_thread, NULL)))
+			printf("\nWaiting for Feed in Thread.");
 
 //---------------------------------------------------------------------------  ~RD
 
--- rss2.h
+++ rss2.h
@@ -10,11 +10,12 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <pthread.h>
+#include "protocol_meta.h"
 
-#define TIMEOUT 1500000
+#define RSS_TIMEOUT 2
 #define MAX_ITEMS 50
 #define BYTEBUFFER 65535
-#define PORT "80"
+#define RSS_PORT "80"
 
 
 //todo: depending on access method a mutex might be required for items_t !
@@ -28,19 +29,17 @@ typedef struct {
 
 typedef struct {
 	int item_count;
-	item_t* item;		
-} items_t;
-
-//global variables allows the changing of source. ~RD
-char* host = "showrss.info";
-char* uri = "/feeds/27.rss";
+	item_t* item;	
+	char* host;
+	char* uri;	
+} rss_t;
 
 /* queried for deletion. Reason: outdated.  ~RD
 // Returns array of all rss items found and the total amount of items
 Items getFeed(char *destAddr, char *dir); */
 
 //public function, is threaded. call with: items_t xxx; rss_feed(&xxx); xxx.item[0].attribute;  ~RD
-void rss_feed(items_t* test);
+void rss_fetch(rss_t* test);
 
 /* queried for deletion. Reason: should not be public. ~RD
 // returns string between two tags
